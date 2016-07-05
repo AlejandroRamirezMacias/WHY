ebadea00e3b81238fcc41e68d524e3638afdefbe
Diego Ramirez <diegor@Diegos-MacBook-Pro.local>
2016-07-04 23:58:35 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once("dream2.html"); ?>
+<?php include_once("dream3.html"); ?>
+<?php include_once("secondPage.html"); ?>
+<?php include_once("homePage.html"); ?>
+<?php include_once("fourthPage.html"); ?>
+<?php include_once("thirdPage.html"); ?>
+<?php include_once("hobbies.html"); ?>
+<?php include_once("fifthPage.html"); ?>
